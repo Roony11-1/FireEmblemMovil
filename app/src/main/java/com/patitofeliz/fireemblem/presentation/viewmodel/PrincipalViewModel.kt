b97176a302ddf2d1b00de34d3f3edd32c711ae25
Roony11-1 <eltheasasin@gmail.com>
2025-11-04 09:42:49 -0300

    tengo que actualizar el sdk hacele downgrade The project is using an incompatible version (AGP 8.12.3) of the Android Gradle plugin. Latest supported version is AGP 8.8.0

--- app/src/main/java/com/patitofeliz/fireemblem/presentation/viewmodel/PrincipalViewModel.kt
+++ app/src/main/java/com/patitofeliz/fireemblem/presentation/viewmodel/PrincipalViewModel.kt
@@ -1,9 +1,24 @@
 package com.patitofeliz.fireemblem.presentation.viewmodel
 
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.lifecycle.ViewModel
 import com.patitofeliz.fireemblem.Manager
 
 class PrincipalViewModel : ViewModel()
 {
+    // El adaptador para el LV Principal
+    private var opcionesMenuPrincipal: Array<String> = arrayOf("Hola")
+    // Un Metido auxiliar para cargar el adapter¿?
 
+    // Necesito cargar el adapter basicamente
+    private fun cargarAdapter(arrayMenu:Array<String>): android.widget.ArrayAdapter
+    {
+
+    }
+
+    private fun cargarAdapterListView(listView:ListView, adaptador:android.widget.ArrayAdapter)
+    {
+
+    }
 }
